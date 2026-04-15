c319fb475b41ac3ba60c2e6b265a20b3cc684cdc
Guilherme Carvalho <guicarvalhosoares@hotmail.com>
2026-04-15 14:02:23 -0300

    fix: settings.py secret key

--- semi-joias-admin/mysite/settings.py
+++ semi-joias-admin/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m@pr4vg3ty@f@63ty*kamk-ue%=n0qu7&_h3=2ctbfx&^01*9$"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
