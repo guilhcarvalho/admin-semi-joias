86c7323529fd599d73f653f95bfecef20fc9fa9f
Guilherme Carvalho <guicarvalhosoares@hotmail.com>
2026-04-17 12:11:17 -0300

    fix: Adjusting settings in templates

--- semi-joias-admin/clients/services/models_filter.py
+++ semi-joias-admin/clients/services/models_filter.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.db.models.functions import Concat
+from django.db.models import Value
+
 
 class ClienteQuerySet(models.QuerySet):
     def param_filter(self, nome=None, celular=None):
         queryset = self
         if nome:
-            queryset = queryset.filter(first_name__icontains=nome)
+                queryset = queryset.annotate(full_name_f=Concat(
+                'first_name',
+                Value(' '),
+                'last_name'
+                )).filter(full_name_f__icontains=nome)
+                
         if celular:
             queryset = queryset.filter(phone_number__icontains=celular)
+            
         return queryset
